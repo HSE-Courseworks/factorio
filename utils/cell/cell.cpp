b4e99a734efc9a3e951452df5fdf5908fa4c8854
Blxssy <127939423+Blxssy@users.noreply.github.com>
2023-11-04 10:46:01 +0300

    R.savelyev/init (#4)
    * init
    
    * Разбиение файлов на папки
    
    * Изменение gitignore
    
    * Удалил idea
    
    ---------
    
    Co-authored-by: Artem Saratovtsev <46279571+DumDereDum@users.noreply.github.com>

---
+++ utils/cell/cell.cpp
@@ -0,0 +1,24 @@
+#include "cell.h"
+#include "../object/object.h"
+#include "../ground/ground.h"
+
+Cell::Cell() {
+    auto ground = new Ground();
+    objects.push_back(ground);
+}
+
+char Cell::getIcon() {
+    return objects[objects.size() - 1] -> getIcon();
+}
+
+void Cell::setObject(_Object* object) {
+    objects.push_back(object);
+}
+
+void Cell::removeObject(){
+    objects.pop_back();
+}
+
+_Object* Cell::getObject(){
+    return objects[objects.size() - 1];
+}
